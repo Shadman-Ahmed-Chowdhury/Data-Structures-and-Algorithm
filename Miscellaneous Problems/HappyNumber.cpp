47595679f08449a4501eb6ebe41dd6a469019564
Shadman <shadmanchowdhury87@gmail.com>
2020-04-07 12:07:15 +0600

    Updated HappyNumber.cpp

--- Miscellaneous Problems/HappyNumber.cpp
+++ Miscellaneous Problems/HappyNumber.cpp
@@ -1,3 +1,18 @@
+/**
+A happy number is a number defined by the following process:
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy numbers.
+
+Explanation:
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+
+**/
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -31,10 +46,22 @@ int main()
     int n;
     cin >> n ;
     if(isHappy(n)) {
-        cout << "Yes";
+        cout << "Yes" << endl;
     }
     else
-        cout << "No";
+        cout << "No" << endl;
 
     return 0;
 }
+
+/**
+
+Input:
+
+19
+
+Output:
+
+Yes
+
+**/
