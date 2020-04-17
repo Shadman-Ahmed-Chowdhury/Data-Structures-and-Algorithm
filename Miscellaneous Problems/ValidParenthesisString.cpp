df18229d1bd1656a77b5b45bc0cea11247c552ce
Shadman <shadmanchowdhury87@gmail.com>
2020-04-17 11:38:35 +0600

    Valid Parenthesis Problem Added

---
+++ Miscellaneous Problems/ValidParenthesisString.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool checkValidString(string str) {
+    int len = str.size();
+
+    stack <int> open, star;
+    for(int i = 0; i < len; i++) {
+        if(str[i] == '(')
+            open.push(i);
+        else if(str[i] == '*')
+            star.push(i);
+        else {
+            if(!open.empty())
+                open.pop();
+            else if(!star.empty())
+                star.pop();
+            else
+                return false;
+        }
+    }
+
+    while(!open.empty()) {
+        if(star.empty())
+            return false;
+        else if(open.top() < star.top()) {
+            open.pop();
+            star.pop();
+        }
+        else
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    if(checkValidString(str))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+    return 0;
+}
+
