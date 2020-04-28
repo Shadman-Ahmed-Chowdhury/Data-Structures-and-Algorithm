a055673561b3100efd7ea04ec78b039409c7b71c
Shadman <shadmanchowdhury87@gmail.com>
2020-04-28 16:01:36 +0600

    Jump Game Problem added

---
+++ Miscellaneous Problems/JumpGame.cpp
@@ -0,0 +1,69 @@
+/**
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+jump length is 0, which makes it impossible to reach the last index.
+
+**/
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool canJump(vector<int>& nums) {
+    int n = nums.size();
+    int mx = 0;
+    for(int i = 0; i < n; i++) {
+        if(i > mx)
+            return false;
+        mx = max(nums[i] + i, mx);
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector <int> nums;
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        nums.push_back(a);
+    }
+    if(canJump(nums))
+        cout << "Can Jump!\n";
+    else
+        cout << "Can't Jump!\n";
+    return 0;
+}
+
+/**
+
+Input:
+
+5
+3 2 1 0 4
+
+Output:
+
+Can't Jump!
+
+**/
